49cefc8c401a1ea268417c2f51fa6d97455b64e1
Juliana <julianachristello@gmail.com>
2021-06-10 15:11:05 -0300

    atualizando

--- src/server.js
+++ src/server.js
@@ -7,14 +7,15 @@ app.use(cors());
 
  let idCount = 0;
  const posts = [];
-const comments = [];
+ const comments = [];
+ let responseComent = [];
       
 app.get('/posts', (req, res) => {
     res.send(posts);
 });
 
 let post ={};
-
+let counter = 0;
 app.get('/posts/:postId', (req, res) => {
     const id = req.params.postId;
     for(let i = 0; i < posts.length; i++) {
@@ -52,7 +53,7 @@ app.post('/posts', (req, res) => {
         coverUrl: newPost.coverUrl,
         contentPreview: 'Esta é a estrutura de um post esperado pelo front-end',
         content: newPost.content,
-        commentCount: 2
+        commentCount: counter
     }
 
     posts.push(newPostObj);
@@ -74,6 +75,7 @@ app.post('/posts/:id/comments', (req, res) => {
             author: req.body.author,
             content: req.body.content
         }
+    //posts[postId].commentCount= counter + 1;
     comments.push(comment);
     res.send(comment);
 });
@@ -82,18 +84,15 @@ app.post('/posts/:id/comments', (req, res) => {
 
 app.get('/posts/:id/comments', (req, res) => {
     const postId = parseInt(req.params.id);
-    const response = comments.filter(comment => comment.postId === postId)
-    res.send(response);
+    responseComent = comments.filter(comment => comment.postId === postId);
+    posts[postId].commentCount = counter +1
+    res.send(responseComent);
 });
 
 
 
 app.put('/posts/:postId', (req, res) => { 
     const id = parseInt(req.params.postId);
-
-     //tenho um put que 
-    //funciona como um post e não é o correto eu quero atualizar os dados
-    
 
     const index = posts.findIndex((post)=> {
         return post.id === id;
