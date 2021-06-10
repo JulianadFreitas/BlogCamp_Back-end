59fdbd2a058aa527fefd39c7e5b839a5c2e93c52
Juliana <julianachristello@gmail.com>
2021-06-10 14:27:08 -0300

    Edição e comentário

--- src/server.js
+++ src/server.js
@@ -5,26 +5,16 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
- let idCount = 2;
- const posts = [{
-        id: 1,
-         title: 'Hello World',
-         coverUrl: 'https://miro.medium.com/max/1024/1*OohqW5DGh9CQS4hLY5FXzA.png',
-        contentPreview: 'Esta é a estrutura de um post esperado pelo front-end',
-         content: 'Este é o conteúdo do post, o que realmente vai aparecer na página do post...',
-        commentCount: 2
-       },{id: 2,
-         title: 'Hello World',
-         coverUrl: 'https://miro.medium.com/max/1024/1*OohqW5DGh9CQS4hLY5FXzA.png',
-         contentPreview: 'Esta é a estrutura de um post esperado pelo front-end',
-         content: 'Este é o conteúdo do post, o que realmente vai aparecer na página do post...',
-         commentCount: 2}];
-
+ let idCount = 0;
+ const posts = [];
+const comments = [];
       
 app.get('/posts', (req, res) => {
     res.send(posts);
 });
+
 let post ={};
+
 app.get('/posts/:postId', (req, res) => {
     const id = req.params.postId;
     for(let i = 0; i < posts.length; i++) {
@@ -62,7 +52,8 @@ app.post('/posts', (req, res) => {
         coverUrl: newPost.coverUrl,
         contentPreview: 'Esta é a estrutura de um post esperado pelo front-end',
         content: newPost.content,
-        commentCount: 2}
+        commentCount: 2
+    }
 
     posts.push(newPostObj);
     res.send(posts);
@@ -72,6 +63,49 @@ app.post('/posts', (req, res) => {
 
 
 
+
+
+
+app.post('/posts/:id/comments', (req, res) => {
+    const postId = parseInt(req.params.id);
+    const comment = {
+            id: comments.length,
+            postId: postId,
+            author: req.body.author,
+            content: req.body.content
+        }
+    comments.push(comment);
+    res.send(comment);
+});
+
+
+
+app.get('/posts/:id/comments', (req, res) => {
+    const postId = parseInt(req.params.id);
+    const response = comments.filter(comment => comment.postId === postId)
+    res.send(response);
+});
+
+
+
+app.put('/posts/:postId', (req, res) => { 
+    const id = parseInt(req.params.postId);
+
+     //tenho um put que 
+    //funciona como um post e não é o correto eu quero atualizar os dados
+    
+
+    const index = posts.findIndex((post)=> {
+        return post.id === id;
+    })
+
+    const newPost = req.body;
+    posts[index].title = newPost.title; 
+    res.send(posts);
+    console.log('oi');
+});
+
+
 app.listen(4000, () => {
-    console.log("rodagndooi");
+    console.log("rocdbagndgjooi");
 });
